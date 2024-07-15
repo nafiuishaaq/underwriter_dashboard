38abc78a69047ebfae64d30cb39da0c81b058f93
nafiuishaaq <nafiuig@gmail.com>
2024-07-15 14:41:46 +0100

    survey page with progress bar

--- js.js
+++ js.js
@@ -1,17 +1,3 @@
-function showPassword() {
-  let passwordInput = document.getElementById("show-password");
-  let showPassBtn = document.getElementById("show-password-button");
-  if (passwordInput.type == "password") {
-    passwordInput.type = "text";
-    showPassBtn.innerHTML = "HIDE";
-  } else {
-    passwordInput.type = "password";
-    showPassBtn.innerHTML = "SHOW";
-  }
-}
-
-// progress bar script begin here
-
 let progress = 67;
 
 function updateProgress(userProgress) {
